f775dc96444bbc2efa11b39a1c4e67ed96a2a069
Mahmud Hasan Jion <mahmudh.jion02@gmail.com>
2023-06-12 13:50:41 +0100

    update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,62 +1,62 @@
 <template>
-  <div class="container">
-    <div>
-      <section class="hero is-primary has-text-centered">
-        <div class="hero-body">
-          <div class="container">
-            <h1 class="title">Tech Blog</h1>
-            <h2 class="subtitle">An awesome tech blog</h2>
+    <div class="container">
+      <div>
+        <section class="hero is-primary has-text-centered">
+          <div class="hero-body">
+            <div class="container">
+              <h1 class="title">Tech Blog</h1>
+              <h2 class="subtitle">An awesome tech blog</h2>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
 
-      <section class="section">
-        <div class="container">
-          <div class="columns">
-            <div class="column is-three-fifths is-offset-one-fifth">
-              <div v-for="post in posts" :key="post.slug" class="mb-5">
-                <h3 class="title is-4">
-                  <nuxt-link :to="post.slug">
-                    {{ post.title }}
-                  </nuxt-link>
-                </h3>
-                <div>
-                  {{ post.description }}
+        <section class="section">
+          <div class="container">
+            <div class="columns">
+              <div class="column is-three-fifths is-offset-one-fifth">
+                <div v-for="post in posts" :key="post.slug" class="mb-5">
+                  <h3 class="title is-4">
+                    <nuxt-link :to="post.slug">
+                      {{ post.title }}
+                    </nuxt-link>
+                  </h3>
+                  <div>
+                    {{ post.description }}
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </div>
-  </div>
-</template>
+  </template>
 
-<script>
-export default {
-  async asyncData({ $content }) {
-    const posts = await $content('posts').sortBy('createdAt', 'desc').fetch()
+  <script>
+  export default {
+    async asyncData({ $content }) {
+      const posts = await $content('posts').sortBy('createdAt', 'desc').fetch()
 
-    return { posts }
-  },
-}
-</script>
+      return { posts }
+    },
+  }
+  </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 10vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+  <style>
+  .container {
+    margin: 0 auto;
+    min-height: 10vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
 
-h3 a {
-  color: #363636;
-}
+  h3 a {
+    color: #363636;
+  }
 
-h3 a:hover {
-  color: #3273dc;
-}
-</style>
+  h3 a:hover {
+    color: #3273dc;
+  }
+  </style>
