31b7fb66485f97e5192bf66d9c3e1ea38a1a547c
Mahmud Hasan Jion <mahmudh.jion02@gmail.com>
2023-06-12 13:46:31 +0100

    update nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,44 +1,49 @@
 export default {
-  // Global page headers (https://go.nuxtjs.dev/config-head)
-  head: {
-    title: 'Tech Blog',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'An awesome tech blog' }
+    // Global page headers (https://go.nuxtjs.dev/config-head)
+    head: {
+      title: 'Tech Blog',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'An awesome tech blog' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]
+    },
+
+    // Global CSS (https://go.nuxtjs.dev/config-css)
+    css: [],
+
+    // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+    plugins: [],
+
+    // Auto import components (https://go.nuxtjs.dev/config-components)
+    components: true,
+
+    // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+    buildModules: [
+      // https://go.nuxtjs.dev/eslint
+      '@nuxtjs/eslint-module',
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
 
-  // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+    // Modules (https://go.nuxtjs.dev/config-modules)
+    modules: [
+      // https://go.nuxtjs.dev/axios
+      '@nuxtjs/axios',
+      // https://go.nuxtjs.dev/pwa
+      '@nuxtjs/pwa',
+      '@nuxt/content'
+    ],
 
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [],
+    // Axios module configuration (https://go.nuxtjs.dev/config-axios)
+    axios: {},
 
-  // Auto import components (https://go.nuxtjs.dev/config-components)
-  components: true,
+    // Build Configuration (https://go.nuxtjs.dev/config-build)
+    build: {},
 
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
-  ],
-
-  // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
-    '@nuxt/content'
-  ],
-
-  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
-  axios: {},
-
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {},
-}
+    server: {
+      port: 3000, // default: 3000
+      host: '0.0.0.0', // default: localhost
+    }
+  }
